402a1e30a2b06361ae4ac7286501ddc7eef73387
Mohammad96Assaf <mohammadassafb@gmail.com>
2019-10-25 18:26:31 +0200

    Ich habe zwei Fenster erstellt: die Hauptfenster und ein Fenster, das nachdem Klicken auf einer Katogorie in der Hauptfenster gezeigt wird.

---
+++ Ui.Desktop/Category.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace De.HsFlensburg.LernkartenApp001.Ui.Desktop
+{
+    /// <summary>
+    /// Interaktionslogik für Category.xaml
+    /// </summary>
+    public partial class Category : Window
+    {
+        public Category()
+        {
+            InitializeComponent();
+        }
+
+        private void ListView1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged_2(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
