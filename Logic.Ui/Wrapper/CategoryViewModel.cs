d50504cece897663be4da7b20b78bb9cc0a25371
pshenrik <pshenrik7@gmail.com>
2019-12-12 19:28:41 +0100

    merge

--- Logic.Ui/Wrapper/CategoryViewModel.cs
+++ Logic.Ui/Wrapper/CategoryViewModel.cs
@@ -1,13 +1,16 @@
-﻿
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using De.HsFlensburg.LernkartenApp001.Business.Model.BusinessObjects;
 using De.HsFlensburg.LernkartenApp001.Logic.Ui.ViewModels.Common;
 
 namespace De.HsFlensburg.LernkartenApp001.Logic.Ui.Wrapper
 {
-    public class ViewCategoryViewModel : AbstractViewModel
+    public class CategoryViewModel : AbstractViewModel
     {
-        public ViewCategoryViewModel()
+        public CategoryViewModel()
         {
             category = new Category();
         }
