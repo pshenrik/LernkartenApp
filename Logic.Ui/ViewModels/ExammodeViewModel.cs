924cc22f8218d154e3f796d047db6adfd79c0070
Meins <BRME9981@INF.FH-FLENSBURG.DE>
2019-12-12 15:21:40 +0100

    ExamMode Umbenennung, konstruktor hinzugefügt

--- Logic.Ui/ViewModels/ExammodeViewModel.cs
+++ Logic.Ui/ViewModels/ExammodeViewModel.cs
@@ -5,55 +5,67 @@ using System.Text;
 using System.Threading.Tasks;
 using De.HsFlensburg.LernkartenApp001.Business.Model.BusinessObjects;
 using De.HsFlensburg.LernkartenApp001.Logic.Ui.ViewModels.Common;
+using De.HsFlensburg.LernkartenApp001.Logic.Ui.Wrapper;
+
 
 namespace De.HsFlensburg.LernkartenApp001.Logic.Ui.ViewModels
 {
-    public class ExammodeViewModel : AbstractViewModel
-    {
+    
 
-        private Category examCategroy;
-        private Card[] ExamCards;
-        private float time;
-        private int CardAmount;
-        private float QuestionProgress;
-        private float ExamProgress;
-        private int wrongAnswers;
-        private int correctAnswers;
-
-
-
-        public ExammodeViewModel(Category category)
+    
+        public class ExamModeViewModel : AbstractViewModel
         {
-            this.examCategroy = category;
-             
-        }
-        public void setTime(float time)
-        {
-            this.time = time;
-        }
 
-        public int CorrectAnswers
-        {
-            get
+            private Category examCategroy;
+            private Card[] ExamCards;
+            private float time;
+            private int CardAmount;
+            private float QuestionProgress;
+            private float ExamProgress;
+            private int wrongAnswers;
+            private int correctAnswers;
+
+            
+            public ExamModeViewModel()
             {
-                return this.correctAnswers;
+
             }
 
-        }
-
-        public int WrongAnswers
-        {
-            get
+            public ExamModeViewModel(Category category)
             {
-                return this.wrongAnswers;
+                this.examCategroy = category;
+
             }
+            
+            public void setTime(float time)
+            {
+                this.time = time;
+            }
+
+            public int CorrectAnswers
+            {
+                get
+                {
+                    return this.correctAnswers;
+                }
+
+            }
+
+            public int WrongAnswers
+            {
+                get
+                {
+                    return this.wrongAnswers;
+                }
+            }
+
+            private bool ReturnTrue()
+            {
+                return true;
+            }
+
+
         }
+    
 
-        private bool ReturnTrue()
-        {
-            return true;
-        }
-
-
-    }
 }
