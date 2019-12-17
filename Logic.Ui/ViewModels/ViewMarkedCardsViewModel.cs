c6cc14496d4262eeefc01e4a0047670a42de0c69
BrendanMeins <brendan.27@hotmail.de>
2019-12-17 19:44:51 +0100

    eigener Kram

--- Logic.Ui/ViewModels/ViewMarkedCardsViewModel.cs
+++ Logic.Ui/ViewModels/ViewMarkedCardsViewModel.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using De.HsFlensburg.LernkartenApp001.Logic.Ui.ViewModels.Common;
 
 namespace De.HsFlensburg.LernkartenApp001.Logic.Ui.ViewModels
 {
-    public class ViewMarkedCardsViewModel
+    public class ViewMarkedCardsViewModel : AbstractViewModel
     {
     }
 }
