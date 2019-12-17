e528e4ca070d9e84c9420f1d107dcc35d7fc37e4
BrendanMeins <brendan.27@hotmail.de>
2019-12-17 19:38:03 +0100

    Hinzufügen von ViewMakedCards + ViewModel, außerdem implementierung im ViewModelLocator

---
+++ Logic.Ui/ViewModels/ViewMarkedCardsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace De.HsFlensburg.LernkartenApp001.Logic.Ui.ViewModels
+{
+    public class ViewMarkedCardsViewModel
+    {
+    }
+}
